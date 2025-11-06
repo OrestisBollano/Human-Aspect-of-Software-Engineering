8ebacbf9a40f8a2f444fa8b01d28fc86834ac4f2
OrestisBollano <91533929+OrestisBollano@users.noreply.github.com>
2025-11-06 20:54:41 +0100

    Update task1.go

--- task1/task1.go
+++ task1/task1.go
@@ -1,7 +1,8 @@
 package task1
 
-// Add takes two integers and returns their sum.
-func Add(a, b int) int {
-    // TODO: Implement this function
-    return 0
+// Read the Python code in task1/README_TASK.md.
+// Your goal is to translate that Python function into this Go function.
+func FindFirst(haystack []int, needle int) int {
+	// TODO: Implement the Go version of the Python function.
+	return -1
 }
