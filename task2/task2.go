72d4fc5ff936455351d38703ee04ce07274a5e6c
OrestisBollano <91533929+OrestisBollano@users.noreply.github.com>
2025-11-06 20:55:47 +0100

    Update task2.go

--- task2/task2.go
+++ task2/task2.go
@@ -1,7 +1,15 @@
 package task2
 
 // Reverse takes a string and returns it in reverse order.
+// NOTE: This function contains a bug!
 func Reverse(s string) string {
-    // TODO: Implement this function
-    return ""
+	runes := []rune(s)
+	// The bug is in the loop condition.
+	// It stops one character too early, so it fails
+	// to swap the middle letters on odd-length strings
+	// and fails on short strings.
+	for i, j := 0, len(runes)-1; i < j-1; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
