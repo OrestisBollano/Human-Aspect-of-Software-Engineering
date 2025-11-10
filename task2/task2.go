78c6b8b97cf1e4b3f0c79477574f3c3f5cd4d0e0
Skynet <robo@taktile.com>
2025-11-10 11:10:40 +0100

    finished all task

--- task2/task2.go
+++ task2/task2.go
@@ -1,14 +1,20 @@
 package task2
 
 import (
+	"regexp"
 	"strings"
-	"unicode"
 )
 
 // CountLetters takes a string and returns a map of each letter
 // (as a rune) to its count, ignoring case and non-letter characters.
 func CountLetters(s string) map[rune]int {
 	// TODO: Implement the Go version of the Python function.
-	
-	return nil // Replace this
+	m := make(map[rune]int)
+	re := regexp.MustCompile(`[^a-z]`)
+	for _, letter := range strings.ToLower(s) {
+		if !re.MatchString(string(letter)) {
+			m[letter]++
+		}
+	}
+	return m
 }
