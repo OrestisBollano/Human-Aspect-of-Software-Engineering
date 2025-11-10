4622212669ea3978e39be8cabef8a1cd3bcaf5e1
OrestisBollano <91533929+OrestisBollano@users.noreply.github.com>
2025-11-10 10:31:54 +0000

    try

--- task1/task1.go
+++ task1/task1.go
@@ -5,7 +5,14 @@ import "strings"
 // WordCount takes a string, splits it into words,
 // and returns a map of each word to its count.
 func WordCount(s string) map[string]int {
-	// TODO: Implement the Go version of the Python function.
+		// TODO: Implement the Go version of the Python function.
+	
+	m := make(map[string]int)
+	
+	for w := string.SplitN(s, " ") {
+		v1 := m[w]
+		m[w] := v1 + 1
+	}
 
-	return nil // Replace this
+	return m // Replace this
 }
