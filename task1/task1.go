951d9f0aa1f05663f446ea096c5a74b7fb4eb077
Ebrar Güler <99403179+ebrarguler@users.noreply.github.com>
2025-11-10 10:36:57 +0000

    Finished all tasks

--- task1/task1.go
+++ task1/task1.go
@@ -1,11 +1,22 @@
 package task1
 
-import "strings"
+	
+import (
+	"strings"
+)
 
 // WordCount takes a string, splits it into words,
 // and returns a map of each word to its count.
 func WordCount(s string) map[string]int {
 	// TODO: Implement the Go version of the Python function.
-
-	return nil // Replace this
+	m := make(map[string]int)
+	if (len(s)<1) {
+        return m
+    }
+	var s_arr = strings.Split(s, " ")
+	for j := 0; j < len(s_arr); j++ {
+    	var word = s_arr[j]
+		m[word] +=1
+    }
+	return m // Replace this
 }
--- task2/task2.go
+++ task2/task2.go
@@ -1,6 +1,7 @@
 package task2
 
 import (
+	"fmt"
 	"strings"
 	"unicode"
 )
@@ -8,7 +9,11 @@ import (
 // CountLetters takes a string and returns a map of each letter
 // (as a rune) to its count, ignoring case and non-letter characters.
 func CountLetters(s string) map[rune]int {
+	const alpha = "abcdefghijklmnopqrstuvwxyz"
 	// TODO: Implement the Go version of the Python function.
-	
-	return nil // Replace this
+	m := make(map[rune]int)
+	for i := 0; i < len(s); i++ {
+		m[s.ToLower[i]] +=1
+    }
+	return m 
 }
