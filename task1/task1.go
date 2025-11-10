0b45c86a8f6442ac38c90bd36601505bba24976c
julikakrueger <117313727+julikakrueger@users.noreply.github.com>
2025-11-10 10:34:36 +0000

    Finished

--- task1/task1.go
+++ task1/task1.go
@@ -1,11 +1,25 @@
 package task1
 
-import "strings"
+import  "strings"
+	
 
 // WordCount takes a string, splits it into words,
 // and returns a map of each word to its count.
 func WordCount(s string) map[string]int {
 	// TODO: Implement the Go version of the Python function.
 
-	return nil // Replace this
+
+    counts := make(map[string]int)
+	    b := [...]string{strings.Split(s, " ")}
+	
+	for w := range b {
+		if(counts[b[w]]){
+			counts[b[w]] = counts[b[w]] + 1
+		}else{
+			counts[b[w]] = 0
+		}
+
+	}
+	return counts // Replace this
+	
 }
